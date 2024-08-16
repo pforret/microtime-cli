f753620ee72a64a1c5c2f9bcc9efa1d998bce407
Peter Forret <peter@forret.com>
2024-08-16 12:25:56 +0200

    MOD: microtime macrotime microtime.exe microtime.go

--- microtime.go
+++ microtime.go
@@ -1,19 +1,28 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "time"
+	"flag"
+	"fmt"
+	"time"
 )
 
 func main() {
-    diffTime := flag.Float64("diff",0,"Calculate duration from the given time")
-    flag.Parse()
-    now := time.Now()
-    microTime := float64(now.Unix()) + (float64(now.Nanosecond()) / 1000000000)
-    if(*diffTime > 0) {
-         fmt.Printf("%.6f\n",microTime - *diffTime)
-   } else {
-        fmt.Printf("%.6f\n",microTime)
-    }
+	diffTime := flag.Float64("diff", 0, "Calculate duration from the given time")
+	formatCmd := flag.String("format", "", "Output format (options: cmd)")
+	flag.Parse()
+	now := time.Now()
+	microTime := float64(now.Unix()) + (float64(now.Nanosecond()) / 1000000000)
+	if *formatCmd == "cmd" {
+		if *diffTime > 0 {
+			fmt.Printf("set ELAPSED=%.6f\n", microTime-*diffTime)
+		} else {
+			fmt.Printf("set MICROTIME=%.6f\n", microTime)
+		}
+	} else {
+		if *diffTime > 0 {
+			fmt.Printf("%.6f\n", microTime-*diffTime)
+		} else {
+			fmt.Printf("%.6f\n", microTime)
+		}
+	}
 }
